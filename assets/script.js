852742c5a3aebc86c854f98bf617dfb13fc9cb6e
riskalaluna <riskalaluna@gmail.com>
2024-04-06 11:29:37 +0700

    first commit

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+    alert("Ini Video Kenangan SMA");
+})
+
+audio.addEventListener("click", function audio() {
+    alert("Ini Audio Kenangan SMa");
+})
+
+gambar.addEventListener("click", function gambar() {
+    alert("Ini Gambar Kenangan SMA");
+})
